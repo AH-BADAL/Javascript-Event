075ad3999f754ca058b885d6c8398b9e1d9593c4
Akhter Hossen Badal <akterhossenbadal810@gmail.com>
2025-10-27 19:26:32 +0600

    first commit

---
+++ event.js
@@ -0,0 +1,42 @@
+// Option-one:Direct set on the Html Element!
+// Option-one amra use korbo na
+
+// Option-two:add onclick fuction
+// Option-two ta amra beshi use korbo
+
+function makeRed() {
+  document.body.style.background = 'red';
+}
+// option-three:
+const MakeblueButton = document.getElementById('make-blue');
+MakeblueButton.onclick = Makeblue;
+
+function Makeblue() {
+  document.body.style.background ='Blue';
+}
+// Option three onother
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.onclick = makePurple;
+
+function makePurple() {
+  document.body.style.background = 'purple';
+}
+// option-four
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.addEventListener('click', makePink);
+
+function makePink() {
+  document.body.style.background = 'pink';
+}
+// option-four another 
+const Makebuttongreen = document.getElementById('make-green');
+Makebuttongreen.addEventListener('click', function makegreen() {
+  document.body.style.background = 'green';
+})
+
+// final option suggestion 
+document.getElementById('make-tomato').addEventListener('click', function () {
+  document.body.style.background = 'tomato';
+});
+  
+
---
+++ focus.js
@@ -0,0 +1,19 @@
+// javascript learning start now
+
+document.getElementById("button").addEventListener("click", function () {
+  document.getElementById("demo").innerHTML =Date();
+});
+
+
+document.getElementById('buttonX').addEventListener("click", function () {
+  document.getElementById('lorem').innerHTML = Date();
+});
+
+
+function Myfunction() {
+  // document.getElementById('box').innerHTML = "Yes!This syntax is working";
+  document.getElementById('yes').innerHTML = "No! This syntax is not working";
+}
+function Hellojs() {
+  document.getElementById('box').innerHTML = 'This is Akhter Hossen Badal';
+}
